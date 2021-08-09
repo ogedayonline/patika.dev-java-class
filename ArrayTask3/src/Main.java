4bd2f0aec9c1e3af52aaf3d8062c45c34e38a22e
Enes Ogeday YALÇIN <ogeday12@gmail.com>
2021-08-09 14:00:46 +0300

    first commit

---
+++ ArrayTask3/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1};
+        int[] duplicate = new int[array.length];
+        int startIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if ((i != j) && (array[i] == array[j])) {
+                    if (!isFind(duplicate, array[i])) {
+                        duplicate[startIndex++] = array[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int value : duplicate){
+            if (value != 0){
+                System.out.println(value);
+            }
+        }
+    }
+}
